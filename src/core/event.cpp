c80259115588b92797d873f90c55c1fcdd13cead
linux_pesar <zenrahabar@gmail.com>
2022-08-15 18:08:28 +0430

    added event handling in an abstracted way to engine.

---
+++ src/core/event.cpp
@@ -0,0 +1,50 @@
+#include "event.hpp"
+
+Event::EventData::EventData() {}
+
+Event::Event()
+{
+    Reset();
+}
+
+Event::Event( const Event& other )
+{
+    Reset();
+
+    *this = other;
+}
+
+Event::Event( Event&& other )
+{
+    *this = std::forward<Event>(other);
+}
+
+Event& Event::operator=( const Event& other )
+{
+    if( this == &other )
+        return *this;
+
+    m_category = other.m_category;
+    m_type = other.m_type;
+    m_data = other.m_data;
+
+    return *this;
+}
+
+Event& Event::operator=( Event&& other )
+{
+    m_category = other.m_category;
+    m_type = other.m_type;
+    m_data = other.m_data;
+
+    other.m_category = EventCategory::NONE;
+    other.m_type = EventType::NONE;
+    other.m_data.null_event.null = 0;
+}
+
+void Event::Reset()
+{
+    m_category = EventCategory::NONE;
+    m_type = EventType::NONE;
+    m_data.null_event.null = 0;
+}
