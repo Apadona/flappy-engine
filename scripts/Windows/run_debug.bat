84d52217e89781845a7c3db16c2fe2b4beb6505c
linux_pesar <zenrahabar@gmail.com>
2022-08-30 15:51:07 +0430

    removed unsed command that should have been removed long time ago!

--- scripts/Windows/run_debug.bat
+++ scripts/Windows/run_debug.bat
@@ -5,7 +5,6 @@ cd ../..
 if /I "%PROCESSOR_ARCHITECTURE%" == "x86"   set system_architecture=32
 if /I "%PROCESSOR_ARCHITECTURE%" == "AMD64" set system_architecture=64
 
-setlocal 
-    path=dependencies\lib\glfw\32;%path%
+setlocal
     gdb "build/bin/windows/%system_architecture%/engine.exe" %*
 endlocal
