55c453099ab7fb9ea4f70cd65b52f6a7688e5adc
saeed <jack3078u@gmail.com>
2022-11-20 18:57:50 +0330

    wrapped all the code for loading the application inside a class.

--- src/core/entry_point.cpp
+++ src/core/entry_point.cpp
@@ -22,11 +22,16 @@ class EngineInterface
                     PrepareForExit(app);
 
                     CORE_LOG_HINT("Engine Application run successfully!");
+
+                    return true;
                 }
+
+                CORE_LOG_ERROR("could not start the application! exiting.");
+                return false;
             }
 
-            std::cerr << "Could not Initialize one or more modules! exiting.\n";
-            return false;
+                std::cerr << "Could not Initialize one or more modules! exiting.\n";
+                return false;
         }
         
     private:
@@ -118,73 +123,4 @@ int main( int argc, char** argv, char** env )
         return 0; // in case that everything handled fine.
 
     return -1; // in case that something went wrong.
-
-    /*
-    if( !Logger::Init() )
-    {
-        std::cerr << "could not initialize Logger!\n";
-        return -1;
-    }
-
-    InitializeLoggers();
-    
-    if( !glfwInit() )
-    {
-        CORE_LOG_ERROR("cannot initialize glfw!");
-        glfwTerminate();
-        return -1;
-    }
-
-    if( !FontLoader::Init() )
-    {
-        CORE_LOG_ERROR("cannot initalize freetype!");
-        return -1;
-    }
-
-    stbi_set_flip_vertically_on_load(true);
-
-    Application* app = RegisterApplication();
-    
-    if( !app->Init({argc,argv,env}) )
-    {
-        CORE_LOG_ERROR("application initialization failed.\n");
-        return -1;
-    }
-
-    app->m_window = new Window(app->m_width,app->m_height,app->m_title,3,1);
-    if( !app->m_window->IsOpen() )
-    {
-        CORE_LOG_ERROR("cannot create the glfw window!\n");
-        glfwTerminate();
-        return -1;
-    }
-
-    if( !gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) )
-    {
-        CORE_LOG_ERROR("cannot get OpenGL functions!\n");
-        glfwTerminate();
-        return -1;
-    }
-
-    TextureManager::Init();
-
-    app->OnCreate();
-    
-    Timer timer;
-    TickType application_update_time = 0;
-
-    while( app->OnUpdate( static_cast<double>(application_update_time) / 1000000 ) )
-    {
-        application_update_time = timer.GetElapsedTime<MicroSeconds>();
-        timer.Reset();
-    }
-
-    app->OnClose();
-
-    glfwTerminate();
-
-    FontLoader::Terminate();
-
-    CORE_LOG_HINT("Engine Application run successfully!");
-    */
 }
